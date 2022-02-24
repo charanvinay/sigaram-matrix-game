9ac2f8ac9af02af7ceb680d4c18443fcc791d9ed
charanvinay <71263421+charanvinay@users.noreply.github.com>
2022-02-24 12:27:09 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [selected, setselected] = useState({});
+  const matrix = [
+    { id: 0, text: "" },
+    { id: 1, text: "" },
+    { id: 2, text: "" },
+    { id: 3, text: "" },
+    { id: 4, text: "" },
+    { id: 5, text: "" },
+    { id: 6, text: "" },
+    { id: 7, text: "" },
+    { id: 8, text: "" },
+    { id: 9, text: "" },
+    { id: 10, text: "" },
+    { id: 11, text: "" },
+    { id: 12, text: "" },
+    { id: 13, text: "" },
+    { id: 14, text: "" },
+    { id: 15, text: "" },
+  ];
+
+  const handleClick = (id) => {
+    const selectedId = matrix.filter((box) => box.id === id);
+    console.log(id, selectedId.id);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box-container">
+        {matrix.map((box) => (
+          <div className="box" onClick={() => handleClick(box.id)} key={box.id}>
+            {box.text}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
